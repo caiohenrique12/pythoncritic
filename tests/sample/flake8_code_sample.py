54e1796d63d2560d83d92f5c889ef0fc1d9d3a29
Caio <caiohenriqueads@gmail.com>
2023-01-22 19:44:18 -0300

    update tests and update flake8 sample

---
+++ tests/sample/flake8_code_sample.py
@@ -0,0 +1,13 @@
+import re
+import os
+
+
+class CodeSmell:
+
+    def __init__(self):
+        pass
+
+    def checkThisCOde(self):
+        print("Starting ..")
+    def output(self):
+        print("Out ..")
